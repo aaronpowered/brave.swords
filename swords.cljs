867f7662b5839d2ad443eba369fff240fe10c7b2
aron <bobo.linux@gmail.com>
2018-06-04 12:49:33 +0200

    https://www.youtube.com/watch?v=E5yFcdPAGv0

--- swords.cljs
+++ swords.cljs
@@ -20,11 +20,12 @@
            [#"[í]"    "i"]
            ]))
 
-(defn fancy [sample]
+(defn fancy "Recursive Pretty Print limited to maps and vectors."
+  [sample]
  (loop [x [0      []    0    false      2        ]  ]
   (let [  [index  coll  lvl  in-a-str?  line-type] x
-        exist (< index (count sample))
-        c (when exist (nth sample index))
+        character-exist (< index (count sample))
+        c (when character-exist (nth sample index))
         last-c (when-not (= index 0) (nth sample (dec index)))
         prev? (fn [lc nc] (and (= last-c lc) (= c nc)))
         line-type (cond
@@ -48,7 +49,8 @@
   (if c (recur return)
     (apply str coll)))))
 
-(defn deep-merge [v & vs]
+(defn deep-merge "Excellent merge. Credits for Metosin Potpurri" 
+  [v & vs]
   (letfn [(rec-merge [v1 v2]
             (if (and (map? v1) (map? v2))
               (merge-with deep-merge v1 v2)
@@ -65,8 +67,11 @@
 (defn set-display! [id update] (set! (.-style.display (by-id id)) update))
 (defn set-html! [id s] (set! (.-innerHTML (by-id id)) s))
 
-(defn jsx->clj [x] (into {} (for [k (.keys js/Object x)] [k (aget x k)])))
-(defn obj->clj
+(defn jsx->clj "Swiss knife v1"
+  [x] 
+  (into {} (for [k (.keys js/Object x)] [k (aget x k)])))
+
+(defn obj->clj "Swiss knife v2"
   [obj]
   (-> (fn [result key]
         (let [v (aget obj key)]
@@ -75,62 +80,55 @@
             (assoc result key v))))
       (reduce {} (.getKeys goog/object obj))))
 
-(defn user-agent [] (-> js/navigator .-userAgent))
-(defn domain [] (-> js/document .-domain))
-(defn title [] (-> js/document .-title))
-(defn referrer [] (-> js/document .-referrer))
-(defn prev-sites [] (-> js/history .-length))
-(defn client-time [] (.Date js/window))
-(defn timezone [] (/ (.getTimezoneOffset (client-time)) 60))
-(defn protocol [] (-> js/window .-location .-protocol))
-(defn host [] (-> js/window .-location .-host))
-(defn host-name [] (-> js/window .-location .-hostname))
-(defn url-hash [] (-> js/window .-location .-hash))
-(defn url-path [] (-> js/window .-location .-pathname))
-(defn browser [] (-> js/navigator .-appCodeName))
-(defn browser-name [] (-> js/navigator .-appName))
-(defn browser-engine [] (-> js/navigator .-product))
-(defn browser-version [] (-> js/navigator .-appVersion))
-(defn browser-plugins [] (obj->clj (-> js/navigator .-plugins)))
-(defn browser-mimes [] (obj->clj (-> js/navigator .-mimeTypes)))
-(defn browser-lang [] (-> js/navigator .-language))
-(defn browser-languages [] (js->clj (-> js/navigator .-languages)))
-(defn charset [] (-> js/document .-charset))
-(defn online? [] (-> js/navigator .-onLine))
-(defn connection [] (obj->clj (-> js/navigator .-connection)))
-(defn os [] (-> js/navigator .-platform))
-(defn cpu-cores [] (-> js/navigator .-hardwareConcurrency))
-(defn performance-timing [] (obj->clj (-> js/window .-performance .-timing)))
-(defn performance-memory [] (obj->clj (-> js/window .-performance .-memory)))
-(defn media-devices [] (obj->clj (-> js/navigator .-mediaDevices)))
-(defn cookies? [] (-> js/navigator .-cookieEnabled))
-(defn cookies [] (-> js/document .-cookie))
-(defn local-storage [] (js/localStorage))
-(defn viewport [] (obj->clj (-> js/document .-visualViewport)))
-(defn screen-width [] (-> js/screen .-width))
-(defn screen-height [] (-> js/screen .-height))
-(defn available-width [] (-> js/screen .-availWidth))
-(defn mobile? [] (< (available-width) 768))
-(defn tablet? [] (= (available-width) 768))
-(defn pc? [] (> (available-width) 768))
-(defn available-height [] (-> js/screen .-availHeight))
-(defn color-depth [] (-> js/screen .-colorDepth))
-(defn pixel-depth [] (-> js/screen .-pixelDepth))
+(def client-profile "Everything I know about"
+  {:useragent (-> js/navigator .-userAgent)
+   :domain (-> js/document .-domain)
+   :title (-> js/document .-title)
+   :referrer (-> js/document .-referrer)
+   :prevsites (-> js/history .-length)
+   :clienttime (.Date js/window)
+   :timezone (/ (.getTimezoneOffset (client-time)) 60)
+   :protocol (-> js/window .-location .-protocol)
+   :host (-> js/window .-location .-host)
+   :hostname (-> js/window .-location .-hostname)
+   :hash (-> js/window .-location .-hash)
+   :path (-> js/window .-location .-pathname)
+   :browser (-> js/navigator .-appCodeName)
+   :browsername (-> js/navigator .-appName)
+   :browserengine (-> js/navigator .-product)
+   :browserversion (-> js/navigator .-appVersion)
+   :browserplugins (obj->clj (-> js/navigator .-plugins))
+   :browsermimes (obj->clj (-> js/navigator .-mimeTypes))
+   :lang (-> js/navigator .-language)
+   :browser-languages (js->clj (-> js/navigator .-languages))
+   :charset (-> js/document .-charset)
+   :online? (-> js/navigator .-onLine)
+   :connection (obj->clj (-> js/navigator .-connection))
+   :os (-> js/navigator .-platform)
+   :cpucores (-> js/navigator .-hardwareConcurrency)
+   :performancetiming (obj->clj (-> js/window .-performance .-timing))
+   :performancememory (obj->clj (-> js/window .-performance .-memory))
+   :mediadevices (obj->clj (-> js/navigator .-mediaDevices))
+   :cookies? (-> js/navigator .-cookieEnabled)
+   :cookies (-> js/document .-cookie)
+   :localstorage (js/localStorage)
+   :viewport (obj->clj (-> js/document .-visualViewport))
+   :screenwidth (-> js/screen .-width)
+   :screenheight (-> js/screen .-height)
+   :availablewidth (-> js/screen .-availWidth)
+   :availableheight (-> js/screen .-availHeight)
+   :colordepth (-> js/screen .-colorDepth)
+   :pixeldepth (-> js/screen .-pixelDepth)})
+
 (def check-in 
-  {:path (url-path)
-   :hash (url-hash)
-   :referrer (referrer)})
+  (select-keys [:path :hash :referrer] 
+               client-profile))
+
 (def device-info 
-  {:os (os)
-   :lang (browser-lang)
-   :cpucores (cpu-cores)
-   :useragent (user-agent)
-   :screenwidth (screen-width)
-   :screenheight (screen-height)
-   :availablewidth (available-width)
-   :availableheight (available-height)
-   :colordepth (color-depth)
-   :pixeldepth (pixel-depth)})
+  (select-keys [:os :lang :cpucores :useragent
+                :screenwidth :screenheight :availablewidth :availableheight
+                :colordepth :pixeldepth] 
+               client-profile))
 
 (defn events->chan
   "Given a target DOM element and event type return a channel of
@@ -158,15 +156,11 @@
                    EventType.MOUSEENTER)]
             (go (while true
                  (loop [] (let [[v c] (alts! [hover])]
-                  (if (= c hover)
-                    (do (function)) ;(js/window.alert "Y"))
-                     (recur))))))))
+                  (if (= c hover) (function) (recur))))))))
 
 (defn leave [id function]
       (let [hover (events->chan (js/document.getElementById id)
                    EventType.MOUSELEAVE)]
             (go (while true
                  (loop [] (let [[v c] (alts! [hover])]
-                  (if (= c hover)
-                    (do (function)) ;(js/window.alert "Y"))
-                     (recur))))))))
+                  (if (= c hover) (function) (recur))))))))
